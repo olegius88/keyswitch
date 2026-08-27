1d81aace3cd1511faf884fe7c12ba7d11fd73eca
Oleg Shevchuk <sh.oleg@list.ru>
2026-08-28 01:16:59 +0500

    Fix CI typing dependencies for Windows modules

--- tools/install-typing-tools.sh
+++ tools/install-typing-tools.sh
@@ -5,6 +5,7 @@ target="${1:-.typing}"
 
 python3 -m pip install --upgrade --target "$target" \
   "mypy==2.3.1" \
+  "Pillow==12.3.0" \
   "typing_extensions>=4.6"
 python3 -m pip install --upgrade --target "$target" --no-deps \
   "PyGObject-stubs==2.17.0"
